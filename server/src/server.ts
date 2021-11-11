9b73004d9a7becc867f231289f3bb6954d90c053
DucQuocXa <duc.ngogia310302@gmail.com>
2021-11-11 23:02:00 +0200

    handle products and fix .env

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,24 @@
 import errorHandler from "errorhandler";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 import app from "./app";
-import { MONGODB_URI, PORT } from "./util/secrets";
+//import { MONGODB_URI } from "./util/secrets";
+dotenv.config({ path: ".env" });
 
-const mongoUrl = MONGODB_URI;
+const PORT = process.env.PORT;
+const mongodbUrl = process.env.MONGODB_URI as string;
 
 mongoose
-  .connect(mongoUrl, {
-    //useNewUrlParser: true,
-    //useUnifiedTopology: true,
-    //useFindAndModify: false,
-    //useCreateIndex: true,
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
   })
   .then(() => {
     // Start Express server
-    app.listen(PORT, () => {
-      console.log(`Server running on port: ${PORT}`);
-    });
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
   })
   .catch((error: Error) => {
     console.log(
