a7e485430a8b38a1915ff9ad04d2a0b9097c1c77
DucQuocXa <duc.ngogia310302@gmail.com>
2021-11-15 01:48:17 +0200

    modified

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -1,20 +1,15 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import AccountBoxOutlinedIcon from "@mui/icons-material/AccountBoxOutlined";
-import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 import useStyles from "./style";
 
 export default function NavBar() {
   const classes = useStyles();
-  let history = useHistory();
-  const handleBack = () => {
-    history.push("/products");
-  };
+
   return (
     <AppBar
       sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, boxShadow: "none" }}
